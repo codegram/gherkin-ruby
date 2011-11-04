ea7497b5b890fc86d2b008cf763a736cde5ffc6f
Marc Divins and Josep M. Bach <dev+divins+txus@codegram.com>
2011-11-04 21:21:23 +0100

    AST visitor

--- lib/gherkin/ast.rb
+++ lib/gherkin/ast.rb
@@ -2,25 +2,44 @@ module Gherkin
   module AST
     class Node
       attr_reader :line, :column
+
+      def accept(visitor)
+        name = self.class.name.split('::').last
+        visitor.send("visit_#{name}".to_sym, self)
+      end
     end
 
     class Feature < Node
       attr_reader :name, :scenarios
-      def initialize(name, scenarios)
+
+      include Enumerable
+
+      def initialize(name, scenarios=[])
         @line, @column = name.line_and_column
 
         @name      = name.to_s
         @scenarios = scenarios
       end
+
+      def each
+        @scenarios.each
+      end
     end
 
     class Scenario < Node
       attr_reader :name, :steps
-      def initialize(name, steps)
+
+      include Enumerable
+
+      def initialize(name, steps=[])
         @line, @column = name.line_and_column
 
         @name  = name.to_s
         @steps = steps
+      end
+
+      def each
+        @steps.each
       end
     end
 
---
+++ test/gherkin/ast_test.rb
@@ -0,0 +1,61 @@
+require_relative '../test_helper'
+require 'ostruct'
+
+class MyVisitor
+  def visit_MyNode(my_node)
+    my_node.elements
+  end
+end
+
+class MyNode < Gherkin::AST::Node
+  attr_reader :elements
+  def initialize(name, elements)
+    @name     = name
+    @elements = elements
+  end
+end
+
+module Gherkin
+  module AST
+    describe Node do
+      it 'is visitable' do
+        my_node = MyNode.new('My Node', ['foo', 'bar', 'baz'])
+        visitor = MyVisitor.new
+
+        my_node.accept(visitor).must_equal ['foo', 'bar', 'baz']
+      end
+    end
+
+    [Feature, Scenario, Step].each do |node|
+      describe node do
+        it 'is a Node' do
+          node.ancestors.must_include Node
+        end
+
+        it 'has a line and column' do
+          name = OpenStruct.new(line_and_column: [2, 13])
+          def name.to_s; 'Name'; end
+
+          instance = node.new(name)
+          instance.name.must_equal 'Name'
+          instance.line.must_equal 2
+          instance.column.must_equal 13
+        end
+      end
+    end
+
+    [Feature, Scenario].each do |node|
+      describe node do
+        it 'is Enumerable' do
+          name = OpenStruct.new(line_and_column: [2, 13])
+          def name.to_s; 'Name'; end
+
+          elements = ['foo', 'bar']
+
+          instance = node.new(name, elements)
+          instance.each.to_a.must_equal ['foo', 'bar']
+        end
+      end
+    end
+  end
+end
